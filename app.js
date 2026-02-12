b2b7cb75703d8e93aa34d811c8838f7d43aa7560
rahul kumar <rahulismt491@gmail.com>
2026-02-12 09:29:46 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// // const index= require("./index");
+
+// // console.log(index.addNo(2,3))
+
+// // console.log(index.subNo(2,3))
+
+// //File Handling 
+
+// const fs = require("fs")
+// //scyn....
+// fs.writeFileSync("./text.txt","Hello world");
+
+// //Ascyc
+// fs.writeFile("./text2.txt","Hello world",(err)=>{});
+
+// const result=fs.readFileSync("./contact.txt","utf-8")
+// console.log(result)
+
+// console.log("rahul")
+
+const index= require("./index");
+
+console.log(index.addNo(2,3))
+console.log(index.subNo(2,3))
+ 
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,27 @@
+// function addNo(a,b){
+//     return a+b;
+// }
+// function subNo(a,b){
+//     return a-b;
+// }
+// // console.log(addNo(20,30))
+
+// module.exports={
+//     addNo,
+//     subNo 
+
+// }
+
+
+
+function addNo(a,b){
+    return a+b
+}
+function subNo(a,b){
+    return a-b
+}
+
+module.exports={
+    addNo,
+    subNo
+}
