0b508d382b1654c55527677759fd349b1731a92a
rahul kumar <rahulismt491@gmail.com>
2026-02-16 09:48:44 +0545

    server create

--- app.js
+++ app.js
@@ -8,17 +8,18 @@
 
 const fs = require("fs")
 //in scyn Write file ....
-//fs.writeFileSync("./text.txt","Hello world");
+//fs.writeFileSync("./text01.txt","Hello world this is rahul");
 
 // //Ascyc
-//fs.writeFile("./rahul.txt","Hello world2",(err)=>{});
+ fs.writeFile("./rahul.txt","Hello world2",(err)=>{});
+ 
 
 //in sync Read file 
-// const result=fs.readFileSync("./rahul.txt","utf-8")
+// const result=fs.readFileSync("./text1.txt","utf-8")
 // console.log(result)
 
-// //in Async Read File
-// const result2=fs.readFile("./text2.txt","utf-8",(err,data)=>{
+// // //in Async Read File
+// const result2=fs.readFile("./text.txt","utf-8",(err,data)=>{
 //     if(err){
 //         console.log(err.Error)
 //     }else{
@@ -29,24 +30,40 @@ const fs = require("fs")
 
 //Append File Handling
 // //in Async append 
-// const result3=fs.appendFile("text2.txt","\n this is new file system",(err)=>{
+// const result3=fs.appendFile("text.txt","\n this is new file system",(err)=>{
 //     console.log("result3")
 // })
 // //in sycro
-// const result4=fs.appendFileSync("text.txt","\n this is file handling")
-// console.log(result4)
+const result4=fs.appendFileSync("text.txt","\n this is file handling")
+console.log(result4)
 
 // //Delete File system
 // //in sync :- Blocking code
-// console.log("hello student") 
-// fs.unlinkSync("text.txt")
-// console.log("the file are deleted")
+
+fs.unlinkSync("text.txt")
+console.log("the file are deleted")
 
 // // in asyncro: non-blocking code 
 fs.unlink("rahul.txt",(err)=>{
-    if(err) throw Error
     console.log("the file are deleted")
 })
+
+//in Async create folder
+fs.mkdir('myFolder', (err) => {
+    if (err) {
+        console.error('Error creating folder:', err.message);
+    } else {
+        console.log('Folder created successfully');
+    }
+});
+
+
+try {
+    fs.mkdirSync('myFolderSync');
+    console.log('Folder created successfully (Sync)');
+} catch (err) {
+    console.error('Error creating folder:', err.message);
+}
 
 
 
--- index.js
+++ index.js
@@ -1,27 +1,45 @@
+// // function addNo(a,b){
+// //     return a+b;
+// // }
+// // function subNo(a,b){
+// //     return a-b;
+// // }
+// // // console.log(addNo(20,30))
+
+// // module.exports={
+// //     addNo,
+// //     subNo 
+
+// // }
+
+
+
 // function addNo(a,b){
-//     return a+b;
+//     return a+b
 // }
 // function subNo(a,b){
-//     return a-b;
+//     return a-b
 // }
-// // console.log(addNo(20,30))
 
 // module.exports={
 //     addNo,
-//     subNo 
-
+//     subNo
 // }
+// Import the http module
+const http = require('http');
 
+// Define server hostname and port
+const hostname = '127.0.0.1';
+const port = 3000;
 
+// Create server
+const server = http.createServer((req, res) => {
+    res.statusCode = 200; // OK
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World rahul kumar!\n');
+});
 
-function addNo(a,b){
-    return a+b
-}
-function subNo(a,b){
-    return a-b
-}
-
-module.exports={
-    addNo,
-    subNo
-}
+// Start server
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
