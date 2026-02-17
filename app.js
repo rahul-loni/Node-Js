3e476f6c02b63c0c727bebb86b67d7c6954d0dfd
rahul kumar <rahulismt491@gmail.com>
2026-02-17 10:44:21 +0545

    create server

--- app.js
+++ app.js
@@ -7,48 +7,47 @@
 //File Handling 
 
 const fs = require("fs")
-//in scyn Write file ....
-//fs.writeFileSync("./text01.txt","Hello world this is rahul");
+//in sycn Write file ....:- that is blocking code .
+//fs.writeFileSync("./text.txt","Hello world ");
 
-// //Ascyc
- fs.writeFile("./rahul.txt","Hello world2",(err)=>{});
+// //Ascyc:-Non Blocking Code 
+//fs.writeFile("./rahul.txt","Hello world2",(err)=>{});
  
 
 //in sync Read file 
-// const result=fs.readFileSync("./text1.txt","utf-8")
+// const result=fs.readFileSync("./contact.txt","utf-8")
 // console.log(result)
 
 // // //in Async Read File
-// const result2=fs.readFile("./text.txt","utf-8",(err,data)=>{
+// const result2=fs.readFile("./rahul.txt","utf-8",(err,data)=>{
 //     if(err){
 //         console.log(err.Error)
 //     }else{
-
 //         console.log(data)
 //     }
 // })
 
 //Append File Handling
 // //in Async append 
-// const result3=fs.appendFile("text.txt","\n this is new file system",(err)=>{
-//     console.log("result3")
-// })
+const result3=fs.appendFile("text.txt","\n this is new file system",(err)=>{
+    console.log("result3")
+})
 // //in sycro
 const result4=fs.appendFileSync("text.txt","\n this is file handling")
 console.log(result4)
 
-// //Delete File system
-// //in sync :- Blocking code
+// // //Delete File system
+// // //in sync :- Blocking code
 
 fs.unlinkSync("text.txt")
 console.log("the file are deleted")
 
-// // in asyncro: non-blocking code 
+// // // in asyncro: non-blocking code 
 fs.unlink("rahul.txt",(err)=>{
     console.log("the file are deleted")
 })
 
-//in Async create folder
+// //in Async create folder
 fs.mkdir('myFolder', (err) => {
     if (err) {
         console.error('Error creating folder:', err.message);
--- createServer.js
+++ createServer.js
@@ -1,14 +1,42 @@
-const http=require("http")
+// const http = require("http");
+// const fs = require("fs");
 
-const hostname = '127.100.1000.10';
-const port = 8000;
+// const myServer = http.createServer((req, res) => {
+
+//   const log = `${Date.now()}: ${req.url} New Req Received\n`;
+
+//   fs.appendFile("log.txt", log, (err, data) => {
+
+//     switch (req.url) {
+
+//       case "/":
+//         res.end("HomePage");
+//         break;
+
+//       case "/about":
+//         res.end("About Page");
+//         break;
+
+//       default:
+//         res.end("404 Not Found");
+//         break;
+//     }
+
+//   });
+
+// });
+
+// myServer.listen(8000, () => {
+//   console.log("Server started on port 8000");
+// });
 
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const http = require("http");
+
+// const myServer = http.createServer((req, res) => {
+//   console.log("New Req Rec.");
+//   res.end("Hello From Server");
+// });
+
+// myServer.listen(8000, () => console.log("Server Started!"));
+
